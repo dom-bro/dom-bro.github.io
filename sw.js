3919d6d9147f420e8add1954f4573b2dcf277fc6
liyunzhao <516911028@qq.com>
2023-02-12 23:31:12 +0800

    Site updated: 2023-02-12 23:31:11

---
+++ sw.js
@@ -0,0 +1,68 @@
+const DB_NAME = 'GitHubPagesServiceWorker'
+
+function fetchWithTimeout (request) {
+  let timeout = 5 * 1000
+  if (['document', 'script', 'style'].includes(request.destination)) timeout = 20 * 1000
+  const timeoutPromise = timeout => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(new Response('timeout', { status: 504, statusText: 'timeout ' }))
+      }, timeout)
+    })
+  }
+  let response
+  return Promise.race([
+    timeoutPromise(timeout),
+    fetch(request).then(res => {
+      response = res
+      if (res.status) {
+        const newHeaders = new Headers(res.headers)
+        newHeaders.delete('content-security-policy-report-only')
+        newHeaders.delete('content-security-policy')
+
+        response = new Response(res.body, {
+          status: res.status,
+          statusText: res.statusText,
+          headers: newHeaders
+        })
+      }
+      caches.open(DB_NAME).then(storage => {
+        storage.put(request, response.clone())
+      })
+      // console.log('========', request.url, response)
+      return response
+    }).catch(error => {
+      response = new Response(null, {
+        status: 500,
+        statusText: error,
+      })
+      // console.log('-------', request.url, response)
+      return response
+    }).finally(response => {
+    })
+  ])
+}
+
+self.addEventListener('fetch', e => {
+  if (!e.request.url.startsWith(e.request.referrer) || e.request.method === 'POST') return
+  // console.log('>>>>>>>> fetch', e.request.url, e)
+  e.respondWith(
+    caches.open(DB_NAME).then(storage => storage.match(e.request, {
+      ignoreSearch: e.request.destination === 'image',
+    }).then(cachedResponse => {
+      // console.log('<<<<<<<<<< fetch respondWith', e.request.url, cachedResponse)
+      const fetchLatest = fetchWithTimeout(e.request)
+      return cachedResponse || fetchLatest
+    }))
+  )
+})
+
+self.addEventListener('install', e => {
+  console.log('sw install', e, self)
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  console.log('sw activate', e)
+  return self.clients.claim()
+})
